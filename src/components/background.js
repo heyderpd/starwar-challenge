05a1825e9264d20f93a3085c323e893b1027a9da
heyder.dias <heyder.dias@b2wdigital.com>
2017-09-12 15:15:06 -0300

    add scss

---
+++ src/components/background.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import sky from '../images/sky.jpg'
+import zoom from '../images/zoom.jpg'
+
+const LightTravel = () => (
+  <svg>
+    <defs>
+      <filter id="zoom" x="0" y="0" width="100%" height="100%">
+        <feImage xmlnsXlink="http://www.w3.org/1999/xlink" xlinkHref={zoom} result="zoom"></feImage>
+        <feBlend in="SourceGraphic" in2="zoom" mode="lighten"></feBlend>
+      </filter>''
+    </defs>
+  </svg>
+)
+
+const Background = () => (
+  <div>
+    <LightTravel />
+    <img src={sky} className="background" />
+  </div>
+)
+
+export default Background
